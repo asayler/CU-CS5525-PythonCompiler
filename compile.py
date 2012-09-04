463772a55ed5e0eefb5d6d19da4b0d5c972a603d
Andy Sayler <andy.sayler@gmail.com>
2012-09-04 16:31:00 -0600

    Added basic input file handling and argument checking

--- compile.py
+++ compile.py
@@ -4,11 +4,31 @@
 # CU CS5525
 # Python Compiler
 
+"""
+USAGE:
+    compile.py <file path>
+"""
+
 import compiler
 import sys
 
-def main():
-    print("This is a test")
+def main(argv=None):
+
+    # Setup and Check Args
+    if argv is None:
+        argv = sys.argv
+    if len(argv) != 2:
+        sys.stderr.write(str(argv[0]) + " requires two arguments\n")
+        sys.stderr.write(__doc__ + "\n")
+        return 1
+    inputFilePath = str(argv[1])
+    sys.stderr.write(str(argv[0]) + ": inputFilePath = " + inputFilePath + "\n")
+    
+    # Parse inputFile
+    ast = compiler.parseFile(inputFilePath)
+    sys.stderr.write(str(argv[0]) + ": ast = " + str(ast) + "\n")
+    
+    return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
