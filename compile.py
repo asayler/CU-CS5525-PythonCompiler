5cf564b23fd8a7371c7a9b677c723298be09f6ae
Andy Sayler <andy.sayler@gmail.com>
2012-09-04 15:37:06 -0600

    Added compile.py shell

---
+++ compile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Python Compiler
+
+import compiler
+import sys
+
+def main():
+    print("This is a test")
+
+if __name__ == "__main__":
+    main()
