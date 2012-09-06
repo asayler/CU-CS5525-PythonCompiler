3dd938ee961764260cd04314c7dd04f23a905cba
Andy Sayler <andy.sayler@gmail.com>
2012-09-06 00:16:25 -0600

    Added new test cases

---
+++ test/p0/test-p0-3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test 3
+
+print(-input() + input())
+print(-input() + input())
+print(-input() + input())
---
+++ test/p0/test-p0-4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test 4
+
+print(-input() + input() + input())
