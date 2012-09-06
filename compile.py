9146016c803ee5b150b620770cb956fd1d174bb4
Andy Sayler <andy.sayler@gmail.com>
2012-09-06 12:37:43 -0600

    Added Mike's flatten function

--- compile.py
+++ compile.py
@@ -12,7 +12,7 @@ import sys
 import compiler
 from compiler.ast import *
 
-DRAWAST_OFFSET = 15
+DRAWAST_OFFSET = 13
 
 def dim_nodes(n):
     """Function to count number of nodes in ast. Returns (total)"""
@@ -121,7 +121,7 @@ def drawAST(tree, offset="", term=0):
             if(i == 0 and len(offset) != 0):
                 #First Node
                 line = ""
-                for j in range(15-len(str(tree[i]))):
+                for j in range(DRAWAST_OFFSET-len(str(tree[i]))):
                     line = line+"-"
                 sys.stderr.write((line+str(tree[i])).rjust(DRAWAST_OFFSET)+"\n")
             else:
@@ -141,6 +141,174 @@ def drawAST(tree, offset="", term=0):
 def unparse(n):
     raise Exception('unparse() not yet implemented')
 
+temp_counter = -1
+def new_temp(prefix):
+    global temp_counter
+    temp_counter = temp_counter + 1
+    return prefix + str(temp_counter)
+
+def is_leaf(ast):
+    return isinstance(ast, Const) or isinstance(ast, Name)
+
+def flatten(ast):
+    if isinstance(ast, Module):
+        return Module(ast.doc, flatten(ast.node))
+    elif isinstance(ast, Stmt):
+        nodes = map(flatten, ast.nodes)
+        nodes = sum(nodes, [])
+        return Stmt(nodes)
+    elif isinstance(ast, Printnl):
+        nodes = map(flatten, ast.nodes)
+        prints = []
+        for (t,l) in nodes:
+            if not is_leaf(t):
+                temp = new_temp('print')
+                l.append(Assign([AssName(temp, 'OP_ASSIGN')], t))
+                prints.append(Name(temp))
+            else:
+                prints.append(t)
+        stmts = sum([l for (t, l) in nodes], [])
+        return stmts + [Printnl(prints, ast.dest)]
+    elif isinstance(ast, Assign):
+        nodes = map(flatten, ast.nodes)
+        assigns = [t for (t, l) in nodes]
+        stmts = sum([l for (t, l) in nodes], [])
+        targ_node, targ_stmts = flatten(ast.expr)
+        return stmts + targ_stmts + [Assign(assigns, targ_node)]
+    elif isinstance(ast, AssName):
+        return (ast, [])
+    elif isinstance(ast, Discard):
+        expr, stmts = flatten(ast.expr)
+        return stmts + [Discard(expr)]
+    elif isinstance(ast, Const):
+        return (ast, [])
+    elif isinstance(ast, Name):
+        return (ast, [])
+    elif isinstance(ast, Add):
+        lexpr, lstmts = flatten(ast.left)
+        rexpr, rstmts = flatten(ast.right)
+        if not is_leaf(lexpr):
+            temp = new_temp("left")
+            lstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], lexpr))
+            lexpr = Name(temp)
+        if not is_leaf(rexpr):
+            temp = new_temp("right")
+            rstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], rexpr))
+            rexpr = Name(temp)
+        return (Add((lexpr, rexpr)), lstmts + rstmts)
+    elif isinstance(ast, UnarySub):
+        expr, stmts = flatten(ast.expr)
+        if not is_leaf(expr):
+            temp = new_temp("usub")
+            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
+            expr = Name(temp)
+        return (UnarySub(expr), stmts)
+    elif isinstance(ast, CallFunc):
+        expr, stmts = flatten(ast.node)
+        if not is_leaf(expr):
+            temp = new_temp("func")
+            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
+            expr = Name(temp)
+        args_exprs = []
+        args_stmts = []
+        for arg in ast.args:
+            arg_expr, arg_stmts = flatten(arg)
+            if not is_leaf(arg_expr):
+                temp = new_temp("arg")
+                arg_stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], arg_expr))
+                arg_expr = Name(temp)
+            args_exprs.append(arg_expr)
+            args_stmts = args_stmts + arg_stmts
+        return (CallFunc(expr, args_exprs), stmts + args_stmts)
+
+def flattenAst(ast):
+    """A function to flatten an AST"""
+
+    if isinstance(ast, Module):
+        #return a Module
+        return Module(ast.doc, flattenAst(ast.node))
+    elif isinstance(ast, Stmt):
+        # Traverse list of nodes and flatten each
+        nodes = []
+        for n in ast.nodes:
+            nodes.append(flattenAst(n))
+        # Convert list of lists to flat list
+        nodes = sum(nodes, [])
+        # Return a Stmt
+        return Stmt(nodes)
+    elif isinstance(ast, Printnl):
+        # Traverse list of nodes and flatten each
+        nodes = []
+        for n in ast.nodes:
+            nodes.append(flattenAst(n))
+        # Traverse flattened list and build (tree, list)
+        prints = []
+        for (t,l) in nodes:
+            if not is_leaf(t):
+                temp = new_temp('print')
+                l.append(Assign([AssName(temp, 'OP_ASSIGN')], t))
+                prints.append(Name(temp))
+            else:
+                prints.append(t)
+        stmts = sum([l for (t, l) in nodes], [])
+        return stmts + [Printnl(prints, ast.dest)]
+    elif isinstance(ast, Assign):
+        # Traverse list of nodes and flatten each
+        nodes = []
+        for n in ast.nodes:
+            nodes.append(flattenAst(n))
+        assigns = [t for (t, l) in nodes]
+        stmts = sum([l for (t, l) in nodes], [])
+        targ_node, targ_stmts = flattenAst(ast.expr)
+        return stmts + targ_stmts + [Assign(assigns, targ_node)]
+    elif isinstance(ast, AssName):
+        return (ast, [])
+    elif isinstance(ast, Discard):
+        expr, stmts = flattenAst(ast.expr)
+        return stmts + [Discard(expr)]
+    elif isinstance(ast, Const):
+        return (ast, [])
+    elif isinstance(ast, Name):
+        return (ast, [])
+    elif isinstance(ast, Add):
+        lexpr, lstmts = flattenAst(ast.left)
+        rexpr, rstmts = flattenAst(ast.right)
+        if not is_leaf(lexpr):
+            temp = new_temp("left")
+            lstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], lexpr))
+            lexpr = Name(temp)
+        if not is_leaf(rexpr):
+            temp = new_temp("right")
+            rstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], rexpr))
+            rexpr = Name(temp)
+        return (Add((lexpr, rexpr)), lstmts + rstmts)
+    elif isinstance(ast, UnarySub):
+        expr, stmts = flattenAst(ast.expr)
+        if not is_leaf(expr):
+            temp = new_temp("usub")
+            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
+            expr = Name(temp)
+        return (UnarySub(expr), stmts)
+    elif isinstance(ast, CallFunc):
+        expr, stmts = flattenAst(ast.node)
+        if not is_leaf(expr):
+            temp = new_temp("func")
+            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
+            expr = Name(temp)
+        args_exprs = []
+        args_stmts = []
+        for arg in ast.args:
+            arg_expr, arg_stmts = flattenAst(arg)
+            if not is_leaf(arg_expr):
+                temp = new_temp("arg")
+                arg_stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], arg_expr))
+                arg_expr = Name(temp)
+            args_exprs.append(arg_expr)
+            args_stmts = args_stmts + arg_stmts
+        return (CallFunc(expr, args_exprs), stmts + args_stmts)
+    else:
+        raise Exception('Error in astToList: unrecognized AST node')
+    
 
 def main(argv=None):
     """Main Compiler Entry Point Function"""
@@ -166,6 +334,13 @@ def main(argv=None):
     # Draw Tree
     drawAST(ast)
     
+    # Flatten Tree
+    flatAST1 = flatten(ast)
+    drawAST(flatAST1)
+
+    flatAST2 = flattenAst(ast)
+    drawAST(flatAST2)
+
     return 0
 
 if __name__ == "__main__":
