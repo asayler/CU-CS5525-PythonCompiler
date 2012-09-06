57cb8a7ea464c786309af936e52f38da5f144dcc
Andy Sayler <andy.sayler@gmail.com>
2012-09-06 14:01:13 -0600

    Added Mike's compiler (with some tweaks) and file output

--- compile.py
+++ compile.py
@@ -137,89 +137,16 @@ def drawAST(tree, offset="", term=0):
                 #Normal Node
                 sys.stderr.write(" ".rjust(DRAWAST_OFFSET)+"\n")
 
-
-def unparse(n):
-    raise Exception('unparse() not yet implemented')
-
 temp_counter = -1
 def new_temp(prefix):
+    """A function to genertae unique temp var names"""
     global temp_counter
     temp_counter = temp_counter + 1
     return prefix + str(temp_counter)
 
 def is_leaf(ast):
+    """A function to return whether or not a node is a terminal leaf"""
     return isinstance(ast, Const) or isinstance(ast, Name)
-
-def flatten(ast):
-    if isinstance(ast, Module):
-        return Module(ast.doc, flatten(ast.node))
-    elif isinstance(ast, Stmt):
-        nodes = map(flatten, ast.nodes)
-        nodes = sum(nodes, [])
-        return Stmt(nodes)
-    elif isinstance(ast, Printnl):
-        nodes = map(flatten, ast.nodes)
-        prints = []
-        for (t,l) in nodes:
-            if not is_leaf(t):
-                temp = new_temp('print')
-                l.append(Assign([AssName(temp, 'OP_ASSIGN')], t))
-                prints.append(Name(temp))
-            else:
-                prints.append(t)
-        stmts = sum([l for (t, l) in nodes], [])
-        return stmts + [Printnl(prints, ast.dest)]
-    elif isinstance(ast, Assign):
-        nodes = map(flatten, ast.nodes)
-        assigns = [t for (t, l) in nodes]
-        stmts = sum([l for (t, l) in nodes], [])
-        targ_node, targ_stmts = flatten(ast.expr)
-        return stmts + targ_stmts + [Assign(assigns, targ_node)]
-    elif isinstance(ast, AssName):
-        return (ast, [])
-    elif isinstance(ast, Discard):
-        expr, stmts = flatten(ast.expr)
-        return stmts + [Discard(expr)]
-    elif isinstance(ast, Const):
-        return (ast, [])
-    elif isinstance(ast, Name):
-        return (ast, [])
-    elif isinstance(ast, Add):
-        lexpr, lstmts = flatten(ast.left)
-        rexpr, rstmts = flatten(ast.right)
-        if not is_leaf(lexpr):
-            temp = new_temp("left")
-            lstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], lexpr))
-            lexpr = Name(temp)
-        if not is_leaf(rexpr):
-            temp = new_temp("right")
-            rstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], rexpr))
-            rexpr = Name(temp)
-        return (Add((lexpr, rexpr)), lstmts + rstmts)
-    elif isinstance(ast, UnarySub):
-        expr, stmts = flatten(ast.expr)
-        if not is_leaf(expr):
-            temp = new_temp("usub")
-            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
-            expr = Name(temp)
-        return (UnarySub(expr), stmts)
-    elif isinstance(ast, CallFunc):
-        expr, stmts = flatten(ast.node)
-        if not is_leaf(expr):
-            temp = new_temp("func")
-            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
-            expr = Name(temp)
-        args_exprs = []
-        args_stmts = []
-        for arg in ast.args:
-            arg_expr, arg_stmts = flatten(arg)
-            if not is_leaf(arg_expr):
-                temp = new_temp("arg")
-                arg_stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], arg_expr))
-                arg_expr = Name(temp)
-            args_exprs.append(arg_expr)
-            args_stmts = args_stmts + arg_stmts
-        return (CallFunc(expr, args_exprs), stmts + args_stmts)
 
 def flattenAst(ast):
     """A function to flatten an AST"""
@@ -308,7 +235,70 @@ def flattenAst(ast):
         return (CallFunc(expr, args_exprs), stmts + args_stmts)
     else:
         raise Exception('Error in astToList: unrecognized AST node')
-    
+
+def scan_allocs(ast):
+    """A function to locate all unique assignments in an ast"""
+    if isinstance(ast, Module):
+        return scan_allocs(ast.node)
+    elif isinstance(ast, Stmt):
+        return reduce(lambda x,y: x.union(y), map(scan_allocs, ast.nodes), set([]))
+    elif isinstance(ast, Assign):
+        return reduce(lambda x,y: x.union(y), map(scan_allocs, ast.nodes), set([]))
+    elif isinstance(ast, AssName):
+        return set([ast.name])
+    else:
+        return set([])
+
+current_offset = 0
+stack_map = {}
+def allocate(var, size):
+    """A function to add a variable to the stack map"""
+    global current_offset, stack_map
+    current_offset = size + current_offset
+    stack_map[var] = current_offset
+    return current_offset
+
+word_size = 4
+def compile_stmt(ast, value_mode='movl'):
+    """A function to compile an ast to x86"""
+    global stack_map
+    if isinstance(ast, Module):
+        header = ['pushl %ebp',
+                  'movl %esp, %ebp',
+                  ('subl $%d, %%esp' % (len(scan_allocs(ast)) * word_size))]
+        footer = ['movl $0, %eax', 'leave', 'ret']
+        return header + compile_stmt(ast.node) + footer
+    elif isinstance(ast, Stmt):
+        return sum(map(compile_stmt, ast.nodes),[])
+    elif isinstance(ast, Printnl):
+        return compile_stmt(ast.nodes[0]) + ['pushl %eax',
+                                             'call print_int_nl',
+                                             ('addl $%d, %%esp' % word_size)]
+    elif isinstance(ast, Assign):
+        expr_assemb = compile_stmt(ast.expr)
+        offset = allocate(ast.nodes[0].name, word_size)
+        return expr_assemb + [('movl %%eax, -%d(%%ebp)' % offset)]
+    elif isinstance(ast, Discard):
+        return compile_stmt(ast.expr)
+    elif isinstance(ast, Add):
+        return compile_stmt(ast.left) + compile_stmt(ast.right, value_mode='addl')
+    elif isinstance(ast, UnarySub):
+        return ['negl %eax']
+    elif isinstance(ast, CallFunc):
+        return ['call input']
+    elif isinstance(ast, Const):
+        return [('%s %d, %%eax' % (value_mode, ast.value))]
+    elif isinstance(ast, Name):
+        return [('%s -%d(%%ebp), %%eax' % (value_mode, stack_map[ast.name]))]
+    else:
+        raise Exception("Unexpected term: " + str(ast))
+
+def write_to_file(assembly, outputFileName):
+    """Function to write assembly to file"""
+    assembly = '.globl main\nmain:\n\t' + '\n\t'.join(assembly)
+    outputfile = open(outputFileName, 'w+')
+    outputfile.write(assembly + '\n')
+    outputfile.close()
 
 def main(argv=None):
     """Main Compiler Entry Point Function"""
@@ -321,25 +311,41 @@ def main(argv=None):
         sys.stderr.write(__doc__ + "\n")
         return 1
     inputFilePath = str(argv[1])
+    if(inputFilePath[-3:] != ".py"):
+        sys.stderr.write(str(argv[0]) + " input file must be of type *.py\n")
+        return 1
+    outputFilePath = inputFilePath.split('/')
+    outputFileName = (outputFilePath[-1:])[0]
+    outputFileName = outputFileName[:-3] + ".s"
+
     sys.stderr.write(str(argv[0]) + ": inputFilePath = " + inputFilePath + "\n")
+    sys.stderr.write(str(argv[0]) + ": outputFilePath = " + str(outputFileName) + "\n")
     
     # Parse inputFile
     ast = compiler.parseFile(inputFilePath)
-    sys.stderr.write(str(argv[0]) + ": ast = " + str(ast) + "\n")
-
-    # Measure Tree
-    num1 = dim_nodes(ast)
-    sys.stderr.write(str(argv[0]) + ": dim_nodes(ast) = " + str(num1) + "\n")
     
+    # Measure Tree
+    sys.stderr.write(str(argv[0]) + ": dim_nodes(ast) = " + str(dim_nodes(ast)) + "\n")
+    sys.stderr.write(str(argv[0]) + ": allocs = " + str(len(scan_allocs(ast))) + "\n")
+
     # Draw Tree
     drawAST(ast)
     
     # Flatten Tree
-    flatAST1 = flatten(ast)
-    drawAST(flatAST1)
+    flatast = flattenAst(ast)
 
-    flatAST2 = flattenAst(ast)
-    drawAST(flatAST2)
+    # Measure Flat Tree
+    sys.stderr.write(str(argv[0]) + ": dim_nodes(ast) = " + str(dim_nodes(flatast)) + "\n")
+    sys.stderr.write(str(argv[0]) + ": allocs = " + str(len(scan_allocs(flatast))) + "\n")
+
+    # Draw Flat Tree
+    drawAST(flatast)
+
+    # Compile flat tree
+    assembly = compile_stmt(flatast)
+    
+    # Write output
+    write_to_file(assembly, outputFileName)
 
     return 0
 
