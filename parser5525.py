5165e192d469343a9ab6d280d9f1343be9a0c2ac
Andy Sayler <andy.sayler@gmail.com>
2012-09-16 15:38:24 -0600

    Added stmt and module parse rules

--- parser5525.py
+++ parser5525.py
@@ -23,6 +23,52 @@ import ply.yacc as yacc
 from compiler.ast import *
 from lexer5525 import tokens
 
+# Parsing Rules
+
+precedence = (('nonassoc', 'PRINT'),
+              ('nonassoc', 'INPUT'),
+              ('left', 'ADD'),
+              ('right', 'UMINUS'),
+              ('nonassoc', 'LPAREN'))
+              
+def p_module(p):
+    'module : stmt_list'
+    p[0] = Module(None, Stmt(p[1]))
+
+def p_stmt_list_multi(p):
+    'stmt_list : stmt stmt_list'
+    p[0] = [p[1]] + p[2]
+
+def p_stmt_list_single(p):
+    'stmt_list : stmt'
+    p[0] = [p[1]]
+
+def p_stmt_list_none(p):
+    'stmt_list : empty'
+    p[0] = []
+
+def p_stmt_print(p):
+    'stmt : PRINT expr' 
+    p[0] = Printnl([p[2]], None)
+
+def p_stmt_assign(p):
+    'stmt : assignee ASSIGN expr' 
+    p[0] = Assign([p[1]], p[3])
+
+def p_stmt_expr(p):
+    'stmt : expr' 
+    p[0] = Discard(p[1])
+
+def p_assignee(p):
+    'assignee : NAME'
+    p[0] = AssName(p(1), 'OP_ASSIGN')
+
+def p_empty(p):
+    'empty :'
+    pass
+
+# Test Main
+
 def parser5525_TestMain(argv=None):
     """Parser Test Cases"""
 
