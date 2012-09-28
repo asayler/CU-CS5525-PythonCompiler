ddc9d74bd4a36c3fa5d95e75350ec16bba3a6dcb
Andy Sayler <andy.sayler@gmail.com>
2012-09-27 22:47:26 -0600

    Added soem of Jeremy's test cases

---
+++ test/test-p0-spill.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test spill
+
+xi = 1
+x = xi
+yi = 2
+y = yi
+zi = 3
+z = zi
+wi = 4
+w = wi
+ai = 5
+a = ai
+bi = 6
+b = bi
+ci = 7
+c = ci
+di = 8
+d = di
+ei = 9
+e = ei
+fi = 10
+f = fi
+print x + y + z + w + a + b + c + d + e + f
