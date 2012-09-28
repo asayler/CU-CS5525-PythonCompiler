a026617c1d04acc2e3e350d79763f5c50e43541d
Andy Sayler <andy.sayler@gmail.com>
2012-09-28 01:20:36 -0600

    Added new test case

---
+++ test/test-p0-assign_lhs_stack.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test assign_lhs_stack
+
+x = 1 + input()
+y = x + x
+z = y + y
+w = z + z
+a = w + w
+b = a + a
+c = b + b
+d = c + c
+e = d + d
+f = x + y + z + w + a + b + c + d + e
+print c
