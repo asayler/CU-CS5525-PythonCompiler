5b07672f20f33ae080415723b5a172ca0148127b
Andy Sayler <andy.sayler@gmail.com>
2012-10-11 15:53:55 -0600

    Added a few new test cases

---
+++ test/test-p0-7.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test 7
+
+x = 3
+y = 9
---
+++ test/test-p1-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P0
+# Test 7
+
+x = [1, 2, 3]
