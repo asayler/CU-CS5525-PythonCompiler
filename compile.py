ff497dde0dd4fe74b1c4185763461c976232138c
Andy Sayler <andy.sayler@gmail.com>
2012-10-11 17:58:51 -0600

    Switched to visitor based flatten function

--- compile.py
+++ compile.py
@@ -24,6 +24,7 @@ from x86ast import *
 
 # Compiler Stages
 from explicate import *
+from flatten import *
 from x86regalloc import *
 
 # Helper Tools
@@ -31,107 +32,6 @@ from astTools import *
 from graph_ast import *
 
 debug = True
-
-### Flatten Functions ###
-
-temp_counter = -1
-def new_temp(prefix):
-    """A function to generate unique temp var names"""
-    global temp_counter
-    temp_counter = temp_counter + 1
-    return prefix + str(temp_counter)
-
-def is_leaf(ast):
-    """A function to return whether or not a node is a terminal leaf"""
-    return isinstance(ast, Const) or isinstance(ast, Name)
-
-def flattenAst(ast):
-    """A function to flatten an AST"""
-
-    if isinstance(ast, Module):
-        #return a Module
-        return Module(ast.doc, flattenAst(ast.node))
-    elif isinstance(ast, Stmt):
-        # Traverse list of nodes and flatten each
-        nodes = []
-        for n in ast.nodes:
-            nodes.append(flattenAst(n))
-        # Convert list of lists to flat list
-        nodes = sum(nodes, [])
-        # Return a Stmt
-        return Stmt(nodes)
-    elif isinstance(ast, Printnl):
-        # Traverse list of nodes and flatten each
-        nodes = []
-        for n in ast.nodes:
-            nodes.append(flattenAst(n))
-        # Traverse flattened list and build (tree, list)
-        prints = []
-        for (t,l) in nodes:
-            if not is_leaf(t):
-                temp = new_temp('print')
-                l.append(Assign([AssName(temp, 'OP_ASSIGN')], t))
-                prints.append(Name(temp))
-            else:
-                prints.append(t)
-        stmts = sum([l for (t, l) in nodes], [])
-        return stmts + [Printnl(prints, ast.dest)]
-    elif isinstance(ast, Assign):
-        # Traverse list of nodes and flatten each
-        nodes = []
-        for n in ast.nodes:
-            nodes.append(flattenAst(n))
-        assigns = [t for (t, l) in nodes]
-        stmts = sum([l for (t, l) in nodes], [])
-        targ_node, targ_stmts = flattenAst(ast.expr)
-        return stmts + targ_stmts + [Assign(assigns, targ_node)]
-    elif isinstance(ast, AssName):
-        return (ast, [])
-    elif isinstance(ast, Discard):
-        expr, stmts = flattenAst(ast.expr)
-        return stmts + [Discard(expr)]
-    elif isinstance(ast, Const):
-        return (ast, [])
-    elif isinstance(ast, Name):
-        return (ast, [])
-    elif isinstance(ast, Add):
-        lexpr, lstmts = flattenAst(ast.left)
-        rexpr, rstmts = flattenAst(ast.right)
-        if not is_leaf(lexpr):
-            temp = new_temp("left")
-            lstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], lexpr))
-            lexpr = Name(temp)
-        if not is_leaf(rexpr):
-            temp = new_temp("right")
-            rstmts.append(Assign([AssName(temp, 'OP_ASSIGN')], rexpr))
-            rexpr = Name(temp)
-        return (Add((lexpr, rexpr)), lstmts + rstmts)
-    elif isinstance(ast, UnarySub):
-        expr, stmts = flattenAst(ast.expr)
-        if not is_leaf(expr):
-            temp = new_temp("usub")
-            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
-            expr = Name(temp)
-        return (UnarySub(expr), stmts)
-    elif isinstance(ast, CallFunc):
-        expr, stmts = flattenAst(ast.node)
-        if not is_leaf(expr):
-            temp = new_temp("func")
-            stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], expr))
-            expr = Name(temp)
-        args_exprs = []
-        args_stmts = []
-        for arg in ast.args:
-            arg_expr, arg_stmts = flattenAst(arg)
-            if not is_leaf(arg_expr):
-                temp = new_temp("arg")
-                arg_stmts.append(Assign([AssName(temp, 'OP_ASSIGN')], arg_expr))
-                arg_expr = Name(temp)
-            args_exprs.append(arg_expr)
-            args_stmts = args_stmts + arg_stmts
-        return (CallFunc(expr, args_exprs), stmts + args_stmts)
-    else:
-        raise Exception('Error in astToList: unrecognized AST node')
 
 ### Instruction Selection ###
 
@@ -288,16 +188,17 @@ def main(argv=None):
         # Print mono_AST
         sys.stderr.write("mono ast = \n" + str(monoast) + "\n")
         # Graph mono_AST
-        #Graph_ast().writeGraph(monoast, debugFileName)
-        
+        #Graph_ast().writeGraph(monoast, debugFileName)        
         
     # Exit early since nothing past this point is implemented for p1 yet
-    return 0
+    # return 0
     
     # Flatten Tree
-    flatast = flattenAst(ast)
+    flatast = FlattenVisitor().preorder(monoast)
     if(debug):
+        # Print flat_AST
         sys.stderr.write("flat ast = \n" + str(flatast) + "\n")
+
 
     # Compile flat tree
     assembly = instr_select(flatast)
