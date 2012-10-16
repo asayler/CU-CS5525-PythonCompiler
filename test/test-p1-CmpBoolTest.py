13d26e77ad041a9065bf2e49bd8d40dd2402a724
Andy Sayler <andy.sayler@gmail.com>
2012-10-16 12:41:48 -0600

    Added Cmp test cases

---
+++ test/test-p1-CmpBoolTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P1
+# Test 2
+
+print(True == False)
+print(False == True)
+print(True == True)
+print(False == False)
+print(True != False)
+print(False != True)
+print(True != True)
+print(False != False)
---
+++ test/test-p1-CmpIntTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P1
+# Test 2
+
+print(9 == 3)
+print(3 == 9)
+print(9 == 9)
+print(3 == 3)
+print(9 != 3)
+print(3 != 9)
+print(9 != 9)
+print(3 != 3)
