2f2add5ca53f0f3a9eeb6ccc37324afc9a5184d7
Andy Sayler <andy.sayler@gmail.com>
2012-10-16 20:24:35 -0600

    Added 'not' test cases

---
+++ test/test-p1-Not1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P1
+# Test 2
+
+print(not(True))
+print(not(1))
+print(not(1 + 2))
+print(not(True + False))
+print(not(False))
+print(not(0))
+print(not(1 + -1))
+print(not(False + False))
