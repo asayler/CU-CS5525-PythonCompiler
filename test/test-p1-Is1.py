68fe54c2edc697ab627151c52485257971f9231a
Andy Sayler <andy.sayler@gmail.com>
2012-10-16 19:55:15 -0600

    Added 'is' test cases

---
+++ test/test-p1-Is1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Andy Sayler
+# CU CS5525
+# Test Program
+# P1
+# Test 2
+
+print(3 is 9)
+print(True is False)
+print(True is 1)
+print(False is 0)
+print(3 is 3)
+print(True is True)
+print(False is False)
+print(0 is 0)
