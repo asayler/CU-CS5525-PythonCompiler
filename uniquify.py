92bdddbd31527746b303ebd58c2352acccef609e
Anne Gatchell <annegatchell@gmail.com>
2012-10-21 16:23:15 -0600

    Set up the class info for the Uniquify visitor

---
+++ uniquify.py
@@ -0,0 +1,38 @@
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# uniquify.py
+# Visitor Funstions to Uniquify AST
+#
+# Adopted from code by Jeremy Siek, Fall 2012
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
+import sys
+
+# Data Types
+from compiler.ast import *
+from monoast import *
+
+from unitcopy import CopyVisitor
+
+# Helper Types
+from vis import Visitor
+from functionwrappers import *
+from utilities import generate_name
+
+class UniquifyVisitor(CopyVisitor):
+	def __init__(self):
+		super(UniquifyVisitor, self).__init__()
+
+	
