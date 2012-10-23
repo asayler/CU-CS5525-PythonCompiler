5479f0dfe76989204fcae88eca4f82d6b7fad71a
Anne Gatchell <annegatchell@gmail.com>
2012-10-23 14:23:03 -0600

    Refactored function args to allow for two passes through the tree, one for unioning l_vars and filling out env dicts and one for renaming

--- uniquify.py
+++ uniquify.py
@@ -46,72 +46,68 @@ class UniquifyVisitor(CopyVisitor):
     def visitModule(self, n):
         if(debug):
             print 'in module'
-        return Module(n.doc, self.dispatch(n.node,{}), n.lineno)
-        # return Module(n.doc, self.dispatch(n.node), n.lineno)
+        (ast, env) = self.dispatch(n.node, {}, set([]), True)
+        return Module(n.doc, self.dispatch(n.node,env), n.lineno)
 
     # Statements
-    def visitStmt(self, n, env):
+    def visitStmt(self, n, env, union, collect_pass):
         if(debug):
             print 'in stmt, env = ',env
         nodes = []
+
         for s in n.nodes:
             nodes += [self.dispatch(s, env)]
         return Stmt(nodes, n.lineno)
-    # def visitStmt(self, n):
-    #     if(debug):
-    #         print 'YAYAYAY'
-    #     nodes = []
-    #     for s in n.nodes:
-    #         nodes += [self.dispatch(s)]
-    #     return Stmt(nodes, n.lineno)
 
-    def visitPrintnl(self, n, env):
+    def visitPrintnl(self, n, env, union, collect_pass):
         nodes = []
         for node in n.nodes:
             nodes += [self.dispatch(node,env)]
         return Printnl(nodes, n.dest, n.lineno)
 
-    def visitAssign(self, n, env):
+    def visitAssign(self, n, env, union, collect_pass):
         nodes = []
         for node in n.nodes:
             nodes += [self.dispatch(node, env)]
         return Assign(nodes, self.dispatch(n.expr, env), n.lineno)
     
-    def visitDiscard(self, n, env):
+    def visitDiscard(self, n, env, union, collect_pass):
         return Discard(self.dispatch(n.expr, env), n.lineno)
     
-    def visitFunction(self, n, env):
+    def visitFunction(self, n, env, union, collect_pass):
         env1 = copy.deepcopy(env)
+        l_vars = set([])
+
+        in_scope = self.dispatch(n.code, env1, l_vars)
         return Function(n.decorators, n.name, n.argnames, n.defaults,
-                        n.flags, n.doc, self.dispatch(n.code, env1))
+                        n.flags, n.doc, in_scope)
 
     # Terminal Expressions
-
-    def visitConst(self, n, env):
+    def visitConst(self, n, env, union, collect_pass):
         return Const(n.value, n.lineno)
     
 
-    def visitName(self, n, env):
+    def visitName(self, n, env, union, collect_pass):
         return Name(n.name, n.lineno)
 
-    def visitAssName(self, n, env):
+    def visitAssName(self, n, env, union, collect_pass):
         return AssName(n.name, n.flags, n.lineno)
 
     # Non-Terminal Expressions
 
-    def visitLambda(self, n, env):
+    def visitLambda(self, n, env, union, collect_pass):
         return Lambda(n.argnames, n.defaults, n.flags, self.dispatch(n.code, env))
 
-    def visitReturn(self, n, env):
+    def visitReturn(self, n, env, union, collect_pass):
         return Return(self.dispatch(n.value, env))
 
-    def visitList(self, n, env):
+    def visitList(self, n, env, union, collect_pass):
         nodes = []
         for node in n.nodes:
             nodes += [self.dispatch(node, env)]
         return List(nodes, n.lineno)
 
-    def visitDict(self, n, env):
+    def visitDict(self, n, env, union, collect_pass):
         items = []
         for item in n.items:
             key = self.dispatch(item[0],env)
@@ -119,48 +115,48 @@ class UniquifyVisitor(CopyVisitor):
             items += [(key, value)]
         return Dict(items, n.lineno)
 
-    def visitSubscript(self, n, env):
+    def visitSubscript(self, n, env, union, collect_pass):
         expr = self.dispatch(n.expr, env)
         subs = []
         for sub in n.subs:
             subs += [self.dispatch(sub, env)]
         return Subscript(expr, n.flags, subs, n.lineno)
     
-    def visitCompare(self, n, env):
+    def visitCompare(self, n, env, union, collect_pass):
         ops = []
         for op in n.ops:
             newop = (op[0], self.dispatch(op[1], env))
             ops += [newop]
         return Compare(self.dispatch(n.expr, env), ops, n.lineno)
 
-    def visitAdd(self, n, env):
+    def visitAdd(self, n, env, union, collect_pass):
         return Add((self.dispatch(n.left, env), self.dispatch(n.right, env)), n.lineno)
     
-    def visitOr(self, n, env):
+    def visitOr(self, n, env, union, collect_pass):
         nodes = []
         for node in n.nodes:
             nodes += [self.dispatch(node, env)]
         return Or(nodes, n.lineno)
 
-    def visitAnd(self, n, env):
+    def visitAnd(self, n, env, union, collect_pass):
         nodes = []
         for node in n.nodes:
             nodes += [self.dispatch(node, env)]
         return And(nodes, n.lineno)
 
-    def visitNot(self, n, env):
+    def visitNot(self, n, env, union, collect_pass):
         return Not(self.dispatch(n.expr, env), n.lineno)
 
-    def visitUnarySub(self, n, env):
+    def visitUnarySub(self, n, env, union, collect_pass):
         return UnarySub(self.dispatch(n.expr, env), n.lineno)
 
-    def visitIfExp(self, n, env):
+    def visitIfExp(self, n, env, union, collect_pass):
         return IfExp(self.dispatch(n.test, env),
                      self.dispatch(n.then, env),
                      self.dispatch(n.else_, env),
                      n.lineno)    
 
-    def visitCallFunc(self, n, env):
+    def visitCallFunc(self, n, env, union, collect_pass):
         args = []
         for arg in n.args:
             args += [self.dispatch(arg, env)]
