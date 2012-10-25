d72c1d3ec05b306bee61cbca7d8495e7bf1fe6fe
Andy Sayler <andy.sayler@gmail.com>
2012-10-25 11:09:41 -0600

    Updated Slambda member naming scheme: body->code

--- graph_monoast.py
+++ graph_monoast.py
@@ -134,7 +134,7 @@ class Graph_monoast(Graph_ast):
         myid = Graphvis_dot().uniqueid(n)
         lines += Graphvis_dot().lineLabel(myid, ("SLambda(%s)" % n.params))
         lines += Graphvis_dot().linePair(p, myid)
-        lines += self.dispatch(n.body, myid)
+        lines += self.dispatch(n.code, myid)
         return lines
 
     def visitIndirectCallFunc(self, n, p):
