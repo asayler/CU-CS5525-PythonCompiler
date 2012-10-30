caa9537df81e0a4187a2f62505d837aa09daebe5
Andy Sayler <andy.sayler@gmail.com>
2012-10-30 14:28:22 -0600

    Added necessary header to test file

--- test/test-p2-fu1.py
+++ test/test-p2-fu1.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# test-p2-fu1.py
+# Test Case
+# Subset: p2
+# Type: 
+# Tesing: 
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
 def mult(x, y):
     return 0 if x == 0 else (y if x == 1 else y + mult(x + -1, y))
 
