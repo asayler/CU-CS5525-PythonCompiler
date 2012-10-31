9eac9f878264200b5be5f1c686bfa764e7084505
Andy Sayler <andy.sayler@gmail.com>
2012-10-30 22:36:02 -0600

    Added comment header to test cases

--- test/test-p2-lambda2.py
+++ test/test-p2-lambda2.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# test-p2-fun3.py
+# Test Case
+# Subset: p2
+# Type: 
+# Tesing: 
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
 adder = lambda x: (lambda y: input() + x + y)
 add5andinput = adder(5)
 over14 = add5andinput(9)
