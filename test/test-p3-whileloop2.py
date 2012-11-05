787adab24e34239cf14acf2d871a52b13021a546
Andy Sayler <andy.sayler@gmail.com>
2012-11-04 19:12:46 -0700

    Added necessary header to test cases, including missing #!

--- test/test-p3-whileloop2.py
+++ test/test-p3-whileloop2.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# test-p3-whileloop2.py
+# Test Case
+# Subset: p3
+# Type: Student
+# Tesing: while loops
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
 x = 4
 while(input()):
     x = x + 1
