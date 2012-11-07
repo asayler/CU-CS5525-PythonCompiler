9e690a075af1ab5783a26bac602a689f45d12228
Michael Vitousek <mvitousek@klamathfalls.(none)>
2012-11-06 18:25:03 -0700

    added string handling, filling in a bunch of missing cases

---
+++ stringfind.py
@@ -0,0 +1,37 @@
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# explicate.py
+# Visitor Functions to Explicate AST
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
+import sys
+
+# Data Types
+from compiler.ast import *
+from monoast import *
+from set_visitor import *
+
+from unitcopy import CopyVisitor
+
+# Helper Types
+from vis import Visitor
+from functionwrappers import *
+from utilities import generate_name
+
+class StringFindVisitor(SetVisitor):
+    def visitString(self, n):
+        location = generate_name('string')
+        n.location = location
+        return set([(location, n.name)])
