fc79a14c5e09fb6fb4f7db5c3ffdae132952dbe3
Andy Sayler <andy.sayler@gmail.com>
2012-11-09 12:59:43 -0700

    Added standard comment header

--- find_locals.py
+++ find_locals.py
@@ -1,3 +1,23 @@
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# find_locals.py
+# Visitor Funstions to Find Local Variables
+#
+# Adopted from code by Jeremy Siek, Fall 2012
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
 from compiler.ast import *
 from set_visitor import SetVisitor
 
