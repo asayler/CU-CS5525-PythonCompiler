2df2d5c3dbd8555be03b1dc7871efa0f3749ba51
Andy Sayler <andy.sayler@gmail.com>
2012-11-29 20:30:42 -0700

    Chnaged inline function to macro to avoid linking issues

--- code/helper/hashtable_itr.h
+++ code/helper/hashtable_itr.h
@@ -28,20 +28,29 @@ hashtable_iterator(struct hashtable *h);
 /* hashtable_iterator_key
  * - return the value of the (key,value) pair at the current position */
 
+
+#define hashtable_iterator_key(i) i->e->k
+
+/*
 inline void *
 hashtable_iterator_key(struct hashtable_itr *i)
 {
     return i->e->k;
 }
+*/
 
 /*****************************************************************************/
 /* value - return the value of the (key,value) pair at the current position */
 
+#define hashtable_iterator_value(i) i->e->v
+
+/*
 inline void *
 hashtable_iterator_value(struct hashtable_itr *i)
 {
     return i->e->v;
 }
+*/
 
 /*****************************************************************************/
 /* advance - advance the iterator to the next element
