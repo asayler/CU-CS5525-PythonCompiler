3b778b8d051c391cd12fc5b931b6159b9277edb9
Andy Sayler <andy.sayler@gmail.com>
2012-12-02 19:39:49 -0700

    Added propogate pass stub

---
+++ code/propagate.py
@@ -0,0 +1,35 @@
+# CU CS5525
+# Fall 2012
+# Python Compiler
+#
+# propagate.py
+# Visitor Functions to Remove Direct Assignments fro AST
+#
+# Repository:
+#    https://github.com/asayler/CU-CS5525-PythonCompiler
+#
+# By :
+#    Anne Gatchell
+#       http://annegatchell.com/
+#    Andy Sayler
+#       http://www.andysayler.com
+#    Michael (Mike) Vitousek
+#       http://csel.cs.colorado.edu/~mivi2269/
+
+# Helper Tools
+from utilities import generate_name, make_assign
+from copy_visitor import CopyVisitor
+
+from functionwrappers import *
+
+# Data Types
+from pyast import *
+
+class PropagateVisitor(CopyVisitor):
+    
+    def __init__(self):
+        super(PropagateVisitor,self).__init__()
+
+    def preorder(self, tree):
+        self.names = {}
+        return super(SSAVisitor, self).preorder(tree)
