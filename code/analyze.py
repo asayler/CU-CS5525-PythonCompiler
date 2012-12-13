b38094ca13d984539e84c6fafe9f26200197fa67
Andy Sayler <andy.sayler@gmail.com>
2012-12-13 15:11:40 -0700

    Updated Makefile and analyze to supprt new split lli/llc structure

--- code/analyze.py
+++ code/analyze.py
@@ -4,15 +4,18 @@ def basename(f):
     return f[:f.rfind('.')]
 
 testdir = 'build'
-llruntimes = {}
+llcruntimes = {}
+lliruntimes = {}
 llcmptimes = {}
 x86runtimes = {}
 x86cmptimes = {}
 for (root, _, files) in os.walk(testdir):
     for f in files:
         f = os.path.join(root, f)
-        if f.endswith('.llrt'):
-            llruntimes[basename(f)] = open(f, 'r')
+        if f.endswith('.llcrt'):
+            llcruntimes[basename(f)] = open(f, 'r')
+        elif f.endswith('.llirt'):
+            lliruntimes[basename(f)] = open(f, 'r')        
         elif f.endswith('.llct'):
             llcmptimes[basename(f)] = open(f, 'r')
         elif f.endswith('.86rt'):
@@ -20,12 +23,13 @@ for (root, _, files) in os.walk(testdir):
         elif f.endswith('.86ct'):
             x86cmptimes[basename(f)] = open(f, 'r')
 
-total86cmp = 0
-totalllcmp = 0
-total86run = 0
-totalllrun = 0
+total86cmp  = 0
+totalllcmp  = 0
+total86run  = 0
+totalllcrun = 0
+totalllirun = 0
 
-for name in llruntimes:
+for name in llcruntimes:
     if not (name in llcmptimes and name in x86runtimes and name in x86cmptimes):
         continue
     llct = float(llcmptimes[name].readline().rstrip())
@@ -34,7 +38,7 @@ for name in llruntimes:
 
     print '%s compile time ratio: %fx (%fs / %fs)' % (name, 1/cratio, llct, x86ct)
 
-    llrt = float(llruntimes[name].readline().rstrip())
+    llrt = float(llcruntimes[name].readline().rstrip())
     x86rt = float(x86runtimes[name].readline().rstrip())
     rratio = -1 if x86rt == 0 else (llrt / x86rt)
     
@@ -43,10 +47,10 @@ for name in llruntimes:
     total86cmp += x86ct
     totalllcmp += llct
     total86run += x86rt
-    totalllrun += llrt
+    totalllcrun += llrt
 
 cratio = -1 if total86cmp == 0 else (totalllcmp / total86cmp)
-rratio = -1 if total86run == 0 else (totalllrun / total86run)
+rratio = -1 if total86run == 0 else (totalllcrun / total86run)
 
 print 'Overall compile time ratio: %fx' % (1/cratio)
 print 'Overall runtime ratio: %fx' % (1/rratio)
